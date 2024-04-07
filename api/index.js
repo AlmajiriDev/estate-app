7e1b561f725df7155d2ef6fadef1689b6f2bb54d
Awwal Adewuyi <almajiri.dev@gmail.com>
2024-04-07 15:06:00 +0100

    update user api

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -18,7 +19,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
-
+app.use(cookieParser);
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
@@ -28,10 +29,10 @@ app.use("/api/auth", authRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error'
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
-    message
-  })
+    message,
+  });
 });
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { test } from "../controllers/user.controller.js";
+import { test, updateUserById } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/test", test);
+router.post("update/:id", updateUserById);
 
 export default router;
