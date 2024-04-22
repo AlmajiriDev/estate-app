4cdbbdf40e287c4d2dd9fef853dbf3cfec75f0c5
Awwal Adewuyi <almajiri.dev@gmail.com>
2024-04-23 00:07:54 +0100

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,26 @@
-
 export default function About() {
   return (
     <div>
-      
+      <h1 className="text-3xl font-bold mb-4 py-20 px-4 mx-w-6xl mx-auto">
+        About Sahand Estate
+      </h1>
+      <p className="text-slate-700">
+        Sahand Estate: Where Dreams Meet Reality. Discover a world of elegance
+        and comfort with Sahand Estate, where every home tells a story of luxury
+        and sophistication. From sprawling estates to cozy apartments, we curate
+        spaces that elevate your lifestyle. With our commitment to excellence
+        and attention to detail, Sahand Estate brings you unparalleled living
+        experiences. Explore our diverse portfolio and let us help you find the
+        perfect sanctuary to call home. <br />
+        Embark on a journey of opulence and refinement with Sahand Estate. Our
+        passion for crafting exceptional living spaces transcends boundaries,
+        offering you a haven of tranquility amidst the bustling world. From chic
+        urban lofts to serene countryside retreats, each property in our
+        collection is a masterpiece of design and functionality. Trust Sahand
+        Estate to fulfill your aspirations for a life of comfort, style, and
+        unparalleled luxury. Welcome to a world where every corner whispers
+        tales of elegance and sophistication.
+      </p>
     </div>
-  )
+  );
 }
